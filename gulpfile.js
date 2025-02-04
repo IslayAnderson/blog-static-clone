f391702fde93c1813fa1a621c90bdcdca75ac099
Islay Anderson <me@islayanderson.co.uk>
2025-02-04 23:52:04 +0000

    yeah maybe don't do that

--- gulpfile.js
+++ gulpfile.js
@@ -39,7 +39,6 @@ function replaceOldStyle() {
             .src(['*.html', '**/*.html'])
             .pipe(replace('/wp-includes/js/jquery/jquery.min.js', ''))
             .pipe(replace('/wp-includes/js/jquery/jquery-migrate.min.js', ''))
-            .pipe(replace('<img ', '<img loading="lazy" '))
             .pipe(gulp.dest('.'))
     )
 }
